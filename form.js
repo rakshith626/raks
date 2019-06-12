4c417a15f0e03f06feeb0b2744b45d20ce6af51d
Rakshith guptha <40866227+rakshith626@users.noreply.github.com>
2019-06-12 16:11:15 +0530

    Add files via upload

---
+++ form.js
@@ -0,0 +1,6 @@
+
+function validation(){
+alert("password must contain special charactors like @,#and etc...,numbers,alphabetsand length should be atleast 8")
+}
+//run above js function when this js is loaded
+validation('lochan@12');
